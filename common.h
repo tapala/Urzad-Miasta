1d3f91f50f7a02ae78949da2773dc04a845f4e7c
Kacper Wesołowski <kacper.wesolowski@student.pk.edu.pl>
2026-01-07 21:04:36 +0100

    Import bibliotek wymaganych do projektu - (w przyszłości pewnie trzebabędzie dodawać/usunąć)

---
+++ common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_H 
+#define COMMON_H
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/sem.h> 
+#include <sys/msg.h> 
+#include <signal.h> 
+#include <errno.h> 
+#include <time.h> 
+#include <string.h> 
+#include <sys/wait.h>
+
+#endif
