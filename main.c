629c5ebb1abcd56ecff9d6de24a486eea848ba21
Kacper Wesołowski <kacper.wesolowski@student.pk.edu.pl>
2026-01-08 01:43:37 +0100

    Zgubiłem znaki {}

--- main.c
+++ main.c
@@ -6,11 +6,12 @@ void sem_op(int semid, int sem_num, int op) {
     s.sem_num = sem_num; 
     s.sem_op = op; 
     s.sem_flg = 0; 
-    if (semop(semid, &s, 1) == -1) 
+    if (semop(semid, &s, 1) == -1){
         if (errno != EIDRM && errno != EINVAL) {
             perror("semop");
         }
-    } 
+    }
+} 
 void sem_p(int semid, int sem_num) { sem_op(semid, sem_num, -1); } 
 void sem_v(int semid, int sem_num) { sem_op(semid, sem_num, 1); }
 // Logowanie do pliku
@@ -18,5 +19,6 @@ void log_to_file(const char *msg) {
     FILE *f = fopen("raport.txt", "a"); 
     if (f) { 
         fprintf(f, "%s", msg); 
-        fclose(f); } 
-    }
+        fclose(f); 
+    } 
+}W
