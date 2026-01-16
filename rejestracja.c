56d74e5542589770058f9378ad4696ed7533a9df
Kacper Wesołowski <kacper.wesolowski@student.pk.edu.pl>
2026-01-16 21:37:56 +0100

    Reszta logiki biletomatu

--- rejestracja.c
+++ rejestracja.c
@@ -13,4 +13,36 @@ void rejestracja_manager(int shm_id, int sem_id) {
     SharedData *shm = (SharedData*)shmat(shm_id, NULL, 0); 
     int msg_id = msgget(ftok(FTOK_PATH, ID_MSG_BILET), 0600); 
     printf("[REJESTRACJA] System biletowy aktywny.\n");
+
+    while (1) { 
+        sem_p(sem_id, SEM_MUTEX); 
+
+        if (shm->koniec_pracy == 2) { 
+            sem_v(sem_id, SEM_MUTEX); 
+            break; 
+        } 
+
+        // Wyciągamy liczbę w kolejce(do aktywacji biletomatów)
+        int kolejka = shm->kolejka_do_biletow; 
+        int N = MAX_PETENTOW_W_BUDYNKU; 
+        int K = N / 3; 
+        if (K < 1) K = 1; 
+        
+        // Skalowanie 
+        int aktywne = 1; 
+        if (kolejka > 2 * K) aktywne = 3; 
+        else if (kolejka > K) aktywne = 2;
+        
+        // Histereza aktywacji biletomatów
+        if (aktywne == 3 && kolejka < (2.0/3.0)*N) aktywne = 2; 
+        if (aktywne == 2 && kolejka < K) aktywne = 1; 
+
+        shm->liczba_aktywnych_biletomatow = aktywne; 
+        sem_v(sem_id, SEM_MUTEX); 
+        obsluga_biletomatu(msg_id, aktywne); 
+        usleep(200000); 
+        // Cykl pracy maszyny 
+        } 
+
+        shmdt(shm);
 }
