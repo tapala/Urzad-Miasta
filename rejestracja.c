89bf742fd84939802cfb8c12e23cce2fba701d53
Kacper Wesołowski <kacper.wesolowski@student.pk.edu.pl>
2026-01-17 03:12:45 +0100

    Zmiana logiki rejestracji(kas) na mam nadzieję poprawną

--- rejestracja.c
+++ rejestracja.c
@@ -1,48 +1,70 @@
 #include "common.h"
-void obsluga_biletomatu(int id_msg, int count) { 
-    Komunikat msg; 
-    // Biletomat przetwarza tyle biletów, ile jest aktywnych stanowisk w cyklu 
-    for(int i=0; i<count; i++) { 
-        if (msgrcv(id_msg, &msg, sizeof(Komunikat) - sizeof(long), 1, IPC_NOWAIT) != -1) { 
-            msg.mtype = msg.pid_petenta; msgsnd(id_msg, &msg, sizeof(Komunikat) - sizeof(long), 0); 
-        } 
-    } 
+#include <signal.h>
+
+#define MAX_KASY 3
+
+pid_t kasy[MAX_KASY];
+int liczba_kas = 0;
+
+void uruchom_kase(){
+    // Uruchomienie kasy
 }
 
-void rejestracja_manager(int shm_id, int sem_id) { 
-    SharedData *shm = (SharedData*)shmat(shm_id, NULL, 0); 
-    int msg_id = msgget(ftok(FTOK_PATH, ID_MSG_BILET), 0600); 
-    printf("[REJESTRACJA] System biletowy aktywny.\n");
 
-    while (1) { 
-        sem_p(sem_id, SEM_MUTEX); 
+void zamknij_kase() {
+    // Zamykanie kasy
+}
 
-        if (shm->koniec_pracy == 2) { 
-            sem_v(sem_id, SEM_MUTEX); 
-            break; 
-        } 
+void kasa_loop(){
+    // Pętla pracy kasy
+}
 
-        // Wyciągamy liczbę w kolejce(do aktywacji biletomatów)
-        int kolejka = shm->kolejka_do_biletow; 
-        int N = MAX_PETENTOW_W_BUDYNKU; 
-        int K = N / 3; 
-        if (K < 1) K = 1; 
-        
-        // Skalowanie 
-        int aktywne = 1; 
-        if (kolejka > 2 * K) aktywne = 3; 
-        else if (kolejka > K) aktywne = 2;
-        
-        // Histereza aktywacji biletomatów
-        if (aktywne == 3 && kolejka < (2.0/3.0)*N) aktywne = 2; 
-        if (aktywne == 2 && kolejka < K) aktywne = 1; 
+int main() {
+    int shmid = shmget(ftok(FTOK_PATH, ID_SHM), sizeof(SharedData), 0);
+    int semid = semget(ftok(FTOK_PATH, ID_SEM), 2, 0);
 
-        shm->liczba_aktywnych_biletomatow = aktywne; 
-        sem_v(sem_id, SEM_MUTEX); 
-        obsluga_biletomatu(msg_id, aktywne); 
-        usleep(200000); 
-        // Cykl pracy maszyny 
-        } 
+    SharedData *shm = shmat(shmid, NULL, 0);
 
-        shmdt(shm);
+    printf("[REJESTRACJA] System biletowy uruchomiony\n");
+
+    while (1) {
+        sem_p(semid, SEM_MUTEX);
+        int kolejka = shm->kolejka_do_biletow;
+        int status = shm->koniec_pracy;
+        sem_v(semid, SEM_MUTEX);
+
+        if (status == 2) {
+            break;
+        }
+
+        int N = MAX_PETENTOW_W_BUDYNKU;
+        int K = N / 3;
+        if (K < 1) K = 1;
+
+        int docelowe = 1;
+        if (kolejka > 2 * K) docelowe = 3;
+        else if (kolejka > K) docelowe = 2;
+
+        // Histereza(swoją drogą bardzo fajne słówko) kas
+        if (liczba_kas == 3 && kolejka < (2 * N) / 3)
+            docelowe = 2;
+
+        if (liczba_kas == 2 && kolejka < K)
+            docelowe = 1;
+
+        while (liczba_kas < docelowe)
+            uruchom_kase();
+
+        while (liczba_kas > docelowe)
+            zamknij_kase();
+
+    }
+
+    // Zamknięcie wszystkiego
+    while (liczba_kas > 0)
+        zamknij_kase();
+
+    shmdt(shm);
+    printf("[REJESTRACJA] Zamykanie systemu biletowego\n");
+    return 0;
 }
